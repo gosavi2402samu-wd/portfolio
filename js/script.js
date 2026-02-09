43a8cc15fb970b0ea3bc916cd7e68cdb21029b4b
gosavi2402samu-wd <gosavi.2402.samu@gmail.com>
2026-02-09 22:38:54 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,1 +1,8 @@
+// Select the form
+const form = document.getElementById("contactForm");
+
+// Add submit event
+form.addEventListener("submit", function () {
+    alert("Thank you! Your message has been submitted.");
+});
 
